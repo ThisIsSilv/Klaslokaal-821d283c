9c66db82c4df488e4033d993f78812ddba7de7f6
Silvano Bakacak <2119823@talnet.nl>
2019-10-03 11:42:39 +0200

    Uitwerking van de exercise

---
+++ klaslokaal.php
@@ -0,0 +1,14 @@
+<?php
+$array = array(
+    "studentA" => "Kreeft",
+    "studentB" => "Bob",
+    "studentC" => "Klaas",
+    "studentD" => "Man",
+    "studentE" => "Geel",
+    "studentF" => "Driehoek",
+    "studentG" => "kwart voor drie",
+
+
+);
+
+var_dump($array);
